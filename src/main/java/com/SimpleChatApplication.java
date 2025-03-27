100c11ea586213d963f3c7dc210137763b0569f0
ODooHo <engh0205@naver.com>
2025-03-27 16:12:05 +0900

    style: code Style Apply

--- src/main/java/com/SimpleChatApplication.java
+++ src/main/java/com/SimpleChatApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleChatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleChatApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleChatApplication.class, args);
+  }
 }
