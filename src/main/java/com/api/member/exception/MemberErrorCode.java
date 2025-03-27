b618feaaaec3cfed0decd2728b45238a2b2c4bf4
ODooHo <engh0205@naver.com>
2025-03-27 22:30:38 +0900

    chore: 사용자 에러 코드 순서 변경

--- src/main/java/com/api/member/exception/MemberErrorCode.java
+++ src/main/java/com/api/member/exception/MemberErrorCode.java
@@ -8,12 +8,12 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum MemberErrorCode implements ApplicationError {
-	MEMBER_NOT_FOUND("사용자 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-	INVALID_LOGIN_REQUEST("아이디 혹은 비밀번호가 잘못되었습니다.", HttpStatus.UNAUTHORIZED),
-	ALREADY_EXISTS_EMAIL("이미 존재하는 이메일입니다.", HttpStatus.CONFLICT);
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보를 찾을 수 없습니다."),
+	INVALID_LOGIN_REQUEST(HttpStatus.UNAUTHORIZED, "아이디 혹은 비밀번호가 잘못되었습니다."),
+	ALREADY_EXISTS_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
 
-	private final String message;
 	private final HttpStatus status;
+	private final String message;
 
 	@Override
 	public HttpStatus getStatus() {
